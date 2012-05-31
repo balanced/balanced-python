5f9b4419d5acd8478ddc0230f3bd8e37075eea58
Marc Sherry <msherry@ono.local.tld>
2012-05-31 15:31:29 -0700

    Ensure that dashes in tax id are allowed

--- tests/suite.py
+++ tests/suite.py
@@ -35,7 +35,7 @@ TEST_CARDS = {
 PERSON_MERCHANT = {
     'type': 'person',
     'name': 'William James',
-    'tax_id': '393483992',
+    'tax_id': '393-48-3992',              # Should work w/ and w/o dashes
     'street_address': '167 West 74th Street',
     'postal_code': '10023',
     'dob': '1842-01-01',
