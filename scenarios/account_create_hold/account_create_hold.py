f36d06ef42328d534bc8f54271f0c0f0a28e4f30
Parham Negahdar <pnegahdar@gmail.com>
2013-07-10 11:39:48 -0700

    Added account create hold stuff.

---
+++ scenarios/account_create_hold/account_create_hold.py
@@ -0,0 +1,6 @@
+import balanced
+
+balanced.configure("46c08048cd8811e2acae026ba7c1aba6")
+
+account = balanced.Account.find('/v1/marketplaces/TEST-MP29J5STPtZVvnjAFndM0N62/accounts/AC1SUaFl5mNtKGOA3LeO2IxN')
+account.hold(100)
