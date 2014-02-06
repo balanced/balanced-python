6e60a7bb56c50a19b475b64ead045a7d17336ff6
Victor Lin <bornstub@gmail.com>
2014-02-06 14:47:59 +0800

    Fix test_empty_list cannot reproduce bug issue

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -101,9 +101,15 @@ class BasicUseCases(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        api_key = balanced.APIKey().save()
-        balanced.configure(api_key.secret)
+        cls.api_key = balanced.APIKey().save()
+        balanced.configure(cls.api_key.secret)
         cls.marketplace = balanced.Marketplace().save()
+
+    def setUp(self):
+        super(BasicUseCases, self).setUp()
+        # some test might rewrite api_key, so we need to configure it
+        # here again
+        balanced.configure(self.api_key.secret)
 
     def test_create_a_second_marketplace_should_fail(self):
         with self.assertRaises(requests.HTTPError) as exc:
@@ -351,4 +357,8 @@ class BasicUseCases(unittest.TestCase):
         order.credit_to(destination=bank_account, amount=1234)
 
     def test_empty_list(self):
-        balanced.Credit.query.all()
+        balanced.configure(None)
+        api_key = balanced.APIKey().save()
+        balanced.configure(api_key.secret)
+        balanced.Marketplace().save()
+        self.assertEqual(balanced.Credit.query.all(), [])
