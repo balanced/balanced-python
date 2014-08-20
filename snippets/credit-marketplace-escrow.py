e1de5e795316fba9c0d5ff4370aeec7df986489c
richie serna <richie@balancedpayments.com>
2014-08-19 20:39:39 -0700

    escrow snippets

---
+++ snippets/credit-marketplace-escrow.py
@@ -0,0 +1,4 @@
+balanced.Marketplace.mine.owner_customer.bank_accounts[0].credit(
+    amount=2000000,
+    description='Credit from Balanced escrow'
+)
---
+++ snippets/debit-marketplace-escrow.py
@@ -0,0 +1,4 @@
+balanced.Marketplace.mine.owner_customer.bank_accounts[0].debit(
+    amount=2000000,
+    description='Pre-fund Balanced escrow'
+)
