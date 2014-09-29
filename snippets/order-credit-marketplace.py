4cc3cf541c1803efcfcd122331598c44eef71c61
richie serna <richie@balancedpayments.com>
2014-09-29 10:11:01 -0700

    Change name of marketplaceBankAccount

--- snippets/order-credit-marketplace.py
+++ snippets/order-credit-marketplace.py
@@ -1,6 +1,6 @@
-marketplace_account = balanced.Marketplace.mine.owner_customer.bank_accounts[0]
+marketplace_bank_account = balanced.Marketplace.mine.owner_customer.bank_accounts[0]
 order.credit_to(
     amount=2000,
     description="Credit from order escrow to marketplace bank account",
-    destination=marketplace_account
+    destination=marketplace_bank_account
 )
